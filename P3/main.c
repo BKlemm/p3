0e5a6b88579df7307be81b2ef4c72352b21ea7ec
BKlemm <santhyno@vection.de>
2018-11-04 19:29:49 +0100

    Initial Commit

---
+++ P3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  P3
+//
+//  Created by Björn Klemm on 04.11.18.
+//  Copyright © 2018 Björn Klemm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
